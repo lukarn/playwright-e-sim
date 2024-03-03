d9f7ef1868c61f6f5c25e5c40ae041ae48fdcf74
lukarn <lukarn@gmail.com>
2024-03-03 20:42:40 +0100

    use template settings file

--- tests/daily-tasks.spec.ts
+++ tests/daily-tasks.spec.ts
@@ -12,7 +12,7 @@ test.describe('Daily tasks tests.', () => {
 
   test.beforeEach(async ({ page }) => {
     // Load env. settings
-    const appsettings = Appsettings.loadFromFile('appsettings.local.json');
+    const appsettings = Appsettings.loadFromFile('appsettings.json');
 
     // Login to the app
     await page.goto(appsettings.baseUrl);

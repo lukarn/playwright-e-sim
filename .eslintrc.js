fb99f73059bcc5a358fcb0459bd3d1e059dbb073
lukarn <lukarn@gmail.com>
2024-03-09 18:14:55 +0100

    run eslint locally and in pipeline v01

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'playwright'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:playwright/recommended'],
+  // parser: '@typescript-eslint/parser',
+  // plugins: ['@typescript-eslint'],
+  // extends: ['plugin:@typescript-eslint/recommended'],
+  // parser: '@typescript-eslint/parser',
+  // plugins: ['playwright'],
+  // extends: ['plugin:playwright/recommended'],
+  };
